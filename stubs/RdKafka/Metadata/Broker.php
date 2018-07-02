5474caece92a8e3200a0927daf090a7d4023ab1a
Vincent Fahrenholz <vincent.fahrenholz@idealo.de>
2018-07-02 11:22:52 +0200

    add stub for RdKafka/Metadata/Broker and modify RdKafka/Metadata->getBrokers()-Phpdoc in order to reference it

---
+++ stubs/RdKafka/Metadata/Broker.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace RdKafka\Metadata;
+
+class Broker
+{
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+    }
+
+    /**
+     * @return string
+     */
+    public function getHost()
+    {
+    }
+
+    /**
+     * @return int
+     */
+    public function getPort()
+    {
+    }
+}
